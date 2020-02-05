c57ace0170d4bb2b586ca5e5d3eb18838556c9e1
Michał Bednarz <mibed90@gmail.com>
2020-02-05 11:49:51 +0100

    Make action save file locally instead of commiting and provide end to end snapshot generation workflow

--- README.md
+++ README.md
@@ -1,21 +1,40 @@
 # Pull request info action
 
-Put following code in .github/workflows/main.yml
+First make sure that you have the latest version of ContextBuddy sbt plugin added to your project.
+
+Then put the following code in .github/workflows/main.yml
 
 ```
 on:
-  pull_request:
-    types: [closed]
+   pull_request:
+     types: [closed]
 
 jobs:
-  pr_info_action_job:
+  generate_snapshot_job:
     runs-on: ubuntu-latest
-    name: A job to save PR review comments
+    name: Generate and save snapshot
     steps:
-    - name: Save PR comments in .storage
-      id: sample
+    - name: Checkout repository
+      uses: actions/checkout@v2
+    - name: Create temporary file with review comments
       uses: VirtusLab/contextbuddy@gh-actions
       with:
         repo-token: ${{ secrets.GITHUB_TOKEN }}
+    - name: Git config # required by sbt generateSnapshot
+      run: |
+        git config --global user.email "context@buddy.com"
+        git config --global user.name "ContextBuddy"
+    - name: Generate snapshot
+      run: sbt generateSnapshot
+    - name: Save snapshot in storage branch
+      run: |
+        git checkout --orphan contextbuddy/storage
+        git rm -rf .
+        mv /home/runner/.contextbuddy/$(echo "$GITHUB_REPOSITORY" | awk -F / '{print $2}')/storage/snapshots $GITHUB_WORKSPACE
+        git add snapshots
+        git commit -m 'Add snapshots'
+        git push --set-upstream origin contextbuddy/storage -ff
 ```
 
+The action will run on each merged pull request. It will generate snapshot for ContextBuddy and save it in dedicated branch
+called contextbuddy/storage within your github repository.
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core')
 const github = require('@actions/github') 
+const fs = require('fs')
 
 async function run() {
 	try {
@@ -13,7 +14,9 @@ async function run() {
 		} = github
 
 
-		if(!pull_request || !pull_request.merged) return
+		if(!pull_request || !pull_request.merged) {
+			throw new Error('Action should be run on merged pull request event')
+		}
 
 		const commentsUrl = pull_request._links.review_comments.href
 		const commentsEndpoint = commentsUrl.replace('https://api.github.com', '')
@@ -24,27 +27,21 @@ async function run() {
 		})
 
 		const commentsRes = await octokit.request(commentsUrl)
+		
 		const comments = commentsRes.data.map(comment => ({
 			path: comment.path,
 			user: comment.user.login,
 			body: comment.body,
 			line: comment.line
 		}))
-		const content = Buffer.from(JSON.stringify(comments)).toString('base64')
 
-		const storagePath = '.storage'
-		const storageFile = await octokit.request(`GET /repos/${repository.full_name}/contents/${storagePath}`)
-		const storage = {
-		  owner: repository.owner.login,
-		  repo: repository.name,
-		  path: storagePath,
-		  message: 'Add context for PR ' + pull_request.number,
-		  content
-		}
-
-		if(storageFile.status == '200') storage.sha = storageFile.data.sha		
-
-		octokit.repos.createOrUpdateFile(storage)
+		// in current implementation sbt generateSnapshot will look for the file named ReviewComments.json
+		const filePath = 'ReviewComments.json'
+		const fileContent = JSON.stringify(comments)
+		fs.writeFile(filePath, fileContent, (err) => {
+		  if(err) throw err
+		  console.log('The file has been saved!');
+		})
 
 	} catch (error) {
 	  core.setFailed(error.message)
