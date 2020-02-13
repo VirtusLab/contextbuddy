d32dbb46fd368a4f98760219a3941815c0bad6b0
Michał Bednarz <mibed90@gmail.com>
2020-02-13 12:04:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,20 +3,10 @@ const github = require('@actions/github');
 
 async function run() {
 	try {
-		const {
-			context: {
-				payload: {
-					repository: {
-						full_name
-					}
-				}
-			}
-		} = github
-
-		const owner = full_name.replace(/\/.+/, '')
-		const repo = full_name.replace(/.+\//, '')
-
 		const userKey = core.getInput('user-key')
+		const owner = core.getInput('owner')
+		const repo = core.getInput('repo')
+		
 		const octokit = new github.GitHub(userKey)
 
 		const existsRepo = () => octokit.request("HEAD /repos/:owner/contextbuddy-storage", {
